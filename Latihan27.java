7e82998c383f3f273947f8f35bb099ff12cfd3e3
RISMAkhaliAlda <rismakhalida@gmail.com>
2024-11-04 20:32:40 +0700

    Add files via upload

---
+++ Latihan27.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author COR i5 GEN 4
+ */
+import java.util.Scanner;
+public class Latihan27 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Masukkan Kalimat : ");
+        String kalimat = scanner.nextLine();
+        
+        System.out.println("====Hasil Formatting====");
+        System.out.println("Huruf Besar : " + kalimat.toUpperCase());
+        System.out.println("Huruf Kecil : " + kalimat.toLowerCase());
+        
+        System.out.println("BUILD SUCCESSFUL (total time: 2 minutes)");
+    }
+}
